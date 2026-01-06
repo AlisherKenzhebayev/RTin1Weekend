56726137b22f2426c24f091c618276891c2a5448
AlisherKenzhebayev <alisher.kenzhebayev.alumni@nu.edu.kz>
2026-01-06 08:27:47 -0800

    Added vec3

--- RT1Weekend/main.cpp
+++ RT1Weekend/main.cpp
@@ -1,16 +1,26 @@
 ﻿#include "main.h"
+#include "stdint.h"
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image.h>
+#include <stb_image_write.h>
+
+#define N_CHANNELS 3
 
 using namespace std;
 
 int main()
 {
-	int image_width = 256;
-	int image_height = 256;
+	const int image_width = 256;
+	const int image_height = 256;
 
 	// Render
 
 	cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+	uint8_t data[image_width * image_height * N_CHANNELS];
+
+	int index = 0;
 	for (int j = 0; j < image_height; j++)
 	{
 		clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
@@ -24,9 +34,14 @@ int main()
 			int ig = int(255.999 * g);
 			int ib = int(255.999 * b);
 
-			cout << ir << ' ' << ig << ' ' << ib << '\n';
+			data[index]		= ir;
+			data[index + 1]	= ig;
+			data[index + 2]	= ib;
+			index += N_CHANNELS;
 		}
 	}
+
+	stbi_write_jpg("output/image_out.jpg", image_width, image_height, N_CHANNELS, data, 100);
 	std::clog << "\rDone.                 \n";
 	return 0;
 }
---
+++ TODO.md
@@ -0,0 +1,5 @@
+1. CI/CD look up - for now too much time/effort
+-- 2. Add a module for non-ppm creation --
+3. Go through the module in 2 days +- 1 day 
+-- 4. Cleared out confusion on pointers/references, finally --
+5. 
